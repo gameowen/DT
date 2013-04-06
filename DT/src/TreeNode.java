d5792278a48cf2c364da5696011b6304684abd83
GameOwen <gameowencz@gmail.com>
2013-04-06 19:43:14 -0400

    Signed-off-by: GameOwen <gameowencz@gmail.com>

--- DT/src/TreeNode.java
+++ DT/src/TreeNode.java
@@ -4,7 +4,6 @@ import java.util.Enumeration;
 import weka.core.Instance;
 import weka.core.Instances;
 
-
 public class TreeNode {
 	boolean isLeaf = false;
 	TreeNode[] children;
@@ -12,47 +11,47 @@ public class TreeNode {
 	int minObj;
 	int[] distribution;
 	int leafClassIndex;
-	
+
 	public TreeNode(int minObj) {
 		this.minObj = minObj;
 	}
-	
+
 	public void buildTree(Instances data) {
 		storeDistribution(data);
 		System.out.println(Arrays.toString(distribution));
-		
+
 		if (data.size() < 2 * minObj || sameClass(data.numInstances())) {
-			this.isLeaf = true;		
-			leafClassIndex = getMajClass();			
+			this.isLeaf = true;
+			leafClassIndex = getMajClass();
 			return;
-		} 
-		
-		
+		}
+
 		getBestModel(data);
-		
+
 		Instances[] splitedInstances = model.split(data);
-		
+
 		children = new TreeNode[splitedInstances.length];
 		for (int i = 0; i < children.length; i++) {
 			children[i] = new TreeNode(this.minObj);
 			children[i].buildTree(splitedInstances[i]);
 		}
 	}
-	
+
 	public int classifyInstance(Instance i) {
 		return 0;
 	}
-	
+
 	private int getMajClass() {
 		int max = 0;
-		
+
 		for (int i = 0; i < distribution.length; i++) {
-			if (distribution[i] > distribution[max]) max = i;
+			if (distribution[i] > distribution[max])
+				max = i;
 		}
-		
+
 		return max;
 	}
-	
+
 	private void getBestModel(Instances data) {
 		double bestGainRatio = 0;
 		SplitModel bestModel = null;
@@ -68,28 +67,29 @@ public class TreeNode {
 				}
 			}
 		}
-		
+
 		this.model = bestModel;
 	}
-	
+
 	private boolean sameClass(int totalNumInstances) {
 		for (int i = 0; i < distribution.length; i++) {
-			if (distribution[i] == totalNumInstances) return true;
+			if (distribution[i] == totalNumInstances)
+				return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	private void storeDistribution(Instances data) {
 		this.distribution = new int[data.classAttribute().numValues()];
-		//System.out.println(data.classAttribute().numValues());
-		
+		// System.out.println(data.classAttribute().numValues());
+
 		@SuppressWarnings("unchecked")
 		Enumeration<Instance> instances = data.enumerateInstances();
-		
+
 		while (instances.hasMoreElements()) {
 			Instance i = (Instance) instances.nextElement();
-			distribution[(int)i.classValue()]++;
+			distribution[(int) i.classValue()]++;
 		}
 	}
 }
--- DT/src/test.java
+++ DT/src/test.java
@@ -1,20 +1,20 @@
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
-
 public class test {
-	
-	public static void main(String[] args) {		
+
+	public static void main(String[] args) {
 		DataSource source = null;
 		Instances data = null;
 		try {
 			source = new DataSource("trainProdSelection.arff");
 			data = source.getDataSet();
-			if (data.classIndex() == -1) data.setClassIndex(data.numAttributes() - 1);
+			if (data.classIndex() == -1)
+				data.setClassIndex(data.numAttributes() - 1);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		DecisionTree dt = new DecisionTree(3);
 		dt.buildClassfier(data);
 	}
