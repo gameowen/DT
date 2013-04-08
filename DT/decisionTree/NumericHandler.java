c566a291e7e76ab58069e6d0de5bd00dabae4ee4
GameOwen <gameowencz@gmail.com>
2013-04-08 11:28:35 -0400

    Signed-off-by: GameOwen <gameowencz@gmail.com>

---
+++ DT/decisionTree/NumericHandler.java
@@ -0,0 +1,13 @@
+
+public class NumericHandler {
+	private int attIndex;
+	private int minObj;
+	private double splitPoint;
+	private double infoGain;
+	private double gainRatio;
+	private double sumOfWeights;
+	private getSplitByInfoGain split_ig = new  getSplitByInfoGain();
+	private getSplitByGainRatio split_gr = new  getSplitByGainRatio();
+	
+}
+}
